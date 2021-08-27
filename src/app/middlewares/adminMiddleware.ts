ca612f4be3045fbb10a537625c3ace9ca0c21035
Erick Hogarth <xandesteamgames977@gmail.com>
2021-08-27 17:52:28 -0300

    coisa demais

---
+++ src/app/middlewares/adminMiddleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface TokenPayLoad {
+  id: string;
+  role: string;
+  iat: number;
+  exp: number;
+}
+
+export default function adminMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.sendStatus(401);
+  }
+
+  const token = authorization.replace('Bearer', '').trim();
+  try {
+    const data = jwt.verify(token, 'secret message');
+    console.log(data);
+    const { id, role } = data as TokenPayLoad;
+    req.userId = id;
+    
+    if(role !== 'admin'){
+        return res.sendStatus(401);
+    }
+
+    return next();
+  } catch {
+    return res.sendStatus(401);
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import authMiddleware from './app/middlewares/authMiddleware';
 import UserController from './app/controllers/UserController';
 import AuthController from './app/controllers/AuthController';
+import adminMiddleware from './app/middlewares/adminMiddleware';
 
 const router = Router();
 
 router.post('/createuser', UserController.create);
 router.post('/login', AuthController.authenticateLogin);
-router.get('/listusers', UserController.listAll);
+router.get('/listusers', adminMiddleware, UserController.listAll);
 router.get('/finduser', UserController.find);
 router.get('/validate', authMiddleware, UserController.index);
 
